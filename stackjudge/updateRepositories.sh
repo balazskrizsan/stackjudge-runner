77e02f0f9342b7ffca5b1a783bec85958a99e9a9
Balazs Krizsan <krizsan.balazs@gmail.com>
2021-11-28 19:43:54 +0100

    updateRepositories.sh clean

---
+++ stackjudge/updateRepositories.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "== UPDATE all repos from origin/master =========================================================================="
+
+git clone https://github.com/balazskrizsan/stack-judge-frontend.git ./frontend
+git clone https://github.com/balazskrizsan/stack-judge-java.git ./backend
+git clone https://github.com/balazskrizsan/stackjudge-aws.git ./aws
+git clone https://github.com/balazskrizsan/stackjudge-aws-sdk-generator.git ./aws_sdk_generator
