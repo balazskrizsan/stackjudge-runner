281899e8d2ec1de6f3f92bb69204ff6c43f87af6
Balazs Krizsan <krizsan.balazs@gmail.com>
2021-11-29 19:44:48 +0100

    clearStructure.sh added

---
+++ stackjudge/scriprs/clearStructure.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rm -rf ./../frontend
+mkdir  ./../frontend
+
+rm -rf ./../backend
+mkdir  ./../backend
+
+rm -rf ./../aws
+mkdir  ./../aws
+
+rm -rf ./../aws_sdk
+mkdir  ./../aws_sdk
+
+rm -rf ./../aws_sdk_java/code
+
+rm -rf ./../aws_sdk_generator
+mkdir  ./../aws_sdk_generator
