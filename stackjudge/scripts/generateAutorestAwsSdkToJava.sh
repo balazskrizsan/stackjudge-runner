ab40d1ff84094064e6c21cea3a589fe01020e18f
Balazs Krizsan <krizsan.balazs@gmail.com>
2021-12-10 21:39:40 +0100

    generateAutorestAwsSdkToJava.sh updates

--- stackjudge/scripts/generateAutorestAwsSdkToJava.sh
+++ stackjudge/scripts/generateAutorestAwsSdkToJava.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-autorest --java --use:@autorest/java@4.0.41 --input-file:../aws_sdk_java/api.yml --output-folder:../aws_sdk_java/code --namespace:com.kbalazsworks.stackjudge_sdk --pom-file=java/pom.xml --v3
+autorest --java \
+  --use:@autorest/java@4.0.41 \
+  --input-file:../aws_sdk_java/api.yml \
+  --output-folder:../aws_sdk_java/code \
+  --namespace:com.kbalazsworks.stackjudge_aws_sdk \
+  --pom-file=java/pom.xml \
+  --enum-types \
+  --verbose \
+  --debug \
+  --v3
