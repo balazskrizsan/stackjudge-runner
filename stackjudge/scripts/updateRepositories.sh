aa440e6ee1b20edf210f520c5b2d1dfe4e43a015
Balazs Krizsan <krizsan.balazs@gmail.com>
2022-09-19 01:02:18 +0200

    updateRepositories.sh stackjudge_notification added

--- stackjudge/scripts/updateRepositories.sh
+++ stackjudge/scripts/updateRepositories.sh
@@ -8,3 +8,4 @@ git clone https://github.com/balazskrizsan/stack-judge-frontend.git ./../fronten
 git clone https://github.com/balazskrizsan/stack-judge-java.git ./../backend
 git clone https://github.com/balazskrizsan/stackjudge-aws.git ./../aws
 git clone https://github.com/balazskrizsan/stackjudge-aws-sdk-generator.git ./../aws_sdk_generator
+git clone https://github.com/balazskrizsan/stackjudge_notification.git ./../notification
