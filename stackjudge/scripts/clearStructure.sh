9cb3111da093a79de0aee7b2195185493ae3db92
Balazs Krizsan <krizsan.balazs@gmail.com>
2022-11-10 19:07:31 +0100

    Init script updates

--- stackjudge/scripts/clearStructure.sh
+++ stackjudge/scripts/clearStructure.sh
@@ -4,20 +4,20 @@ echo "== Clear folder structure ================================================
 
 rm -rf ./../frontend
 mkdir  ./../frontend
+echo "== Clear: frontend"
 
 rm -rf ./../backend
 mkdir  ./../backend
+echo "== Clear: backend"
 
 rm -rf ./../aws
 mkdir  ./../aws
-
-rm -rf ./../aws_sdk
-mkdir  ./../aws_sdk
-
-rm -rf ./../aws_sdk_java/code
-
-rm -rf ./../aws_sdk_generator
-mkdir  ./../aws_sdk_generator
+echo "== Clear: aws"
 
 rm -rf ./../notification
 mkdir  ./../notification
+echo "== Clear: notification"
+
+rm -rf ./../stackjudge_identity_service
+mkdir  ./../stackjudge_identity_service
+echo "== Clear: stackjudge_identity_service"
--- stackjudge/scripts/updateRepositories.sh
+++ stackjudge/scripts/updateRepositories.sh
@@ -7,5 +7,5 @@ echo "== UPDATE all repos from origin/master ===================================
 git clone https://github.com/balazskrizsan/stack-judge-frontend.git ./../frontend
 git clone https://github.com/balazskrizsan/stack-judge-java.git ./../backend
 git clone https://github.com/balazskrizsan/stackjudge-aws.git ./../aws
-git clone https://github.com/balazskrizsan/stackjudge-aws-sdk-generator.git ./../aws_sdk_generator
 git clone https://github.com/balazskrizsan/stackjudge_notification.git ./../notification
+git clone https://github.com/balazskrizsan/stackjudge_identity_service.git ./../stackjudge_identity_service
